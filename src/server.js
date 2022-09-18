17e30c767982770f9097ded13597a9bf4ac5e599
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-09-18 22:25:45 +0900

    Set js, pug, mvp.css, app.get/app.use

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 import express, { application } from "express";
 
 const app = express();
-console.log("hello");
+const PORT = 3000;
 
-app.listen(3000);
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`🚀 Listening on http://localhost:${PORT}`);
+app.listen(PORT, handleListen);
